dfa85e5bc9ea71a0f5072dfdca08849e6262f497
yoon <dcfdcf4566@gmail.com>
2020-08-05 22:22:23 +0900

    Updates

---
+++ precache-manifest.0715500542136c0bddfd9f8dba34d090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a785d4dcc41fe4aa23442c56e1d28af8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0413a6a01c7203ad26e",
+    "url": "/static/css/main.2370834b.chunk.css"
+  },
+  {
+    "revision": "74f4db23d3107d34211a",
+    "url": "/static/js/2.027fe0c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.027fe0c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0413a6a01c7203ad26e",
+    "url": "/static/js/main.8e59acf0.chunk.js"
+  },
+  {
+    "revision": "a47ae246b1b0ec590bbd",
+    "url": "/static/js/runtime-main.fb2d0c21.js"
+  }
+]);
